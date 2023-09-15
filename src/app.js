6549f69d521da4bc145cc17be005861f6feed998
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-15 09:16:15 -0700

    Added test file for jest

--- src/app.js
+++ src/app.js
@@ -159,3 +159,5 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
+
+module.exports = { editGrocery, deleteItemInGrocery, server };
